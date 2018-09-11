7098c5f62eab9eca1289861b2faf53065fe07e6e
qianbin <cola.tin.com@gmail.com>
2018-09-11 12:20:19 +0800

    feat: support @ symbol to await expression

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,82 @@
-#!/usr/bin/env node
-
 import REPL = require('repl')
 
 declare module 'repl' {
     interface REPLServer {
-        context: any
-        eval: (cmd: string, ctx: any, filename: string, callback: (error: any, result?: any) => void) => void
-        completer: (line: string, callback: (error: any, out?: [string[], string]) => void) => void
-
-        q(query: string): Promise<string>
+        eval: (cmd: string, ctx: any, filename: string, callback: (error?: any, result?: any) => void) => void
+        completer: (line: string, callback: (error: any, out?: [string[], string]) => void) => void    
     }
 }
 
-export function start(options: REPL.ReplOptions, ctxObj: () => object) {
-    const repl = REPL.start(options)
+export interface ServerX extends REPL.REPLServer {  
+    q(query: string): Promise<string>
+}
+
+export function start(options?: string | REPL.ReplOptions): ServerX {
+    const server = REPL.start(options) as ServerX
 
     // promisfied question
-    repl.q = (query) => {
+    server.q = (query) => {
         return new Promise<string>(resolve => {
-            repl.question(query, answer => {
-                resolve(answer)
-            })
+            server.question(query, resolve)
         })
     }
-    const originCompleter = repl.completer;
-    function init() {
 
-        const obj = ctxObj()
+    const suffixesToSkip = Object.getOwnPropertyNames(Object.prototype).map(n => '.' + n)
+    const globalNames = [
+        ...Object.getOwnPropertyNames(server.context),
+        ...Object.getOwnPropertyNames(global),
+        ...Object.getOwnPropertyNames(Object.prototype)
+    ]
 
-        // override completer
-        const propNames = Object.getOwnPropertyNames(obj)
-        const skipSuffixes = Object.getOwnPropertyNames(Object.prototype).map(n => '.' + n)
+    const shouldSkipAutoCompletion = (s: string) => {
+        if (s.indexOf('.') < 0) {
+            return globalNames.indexOf(s) >= 0
+        }
+        return suffixesToSkip.some(n => s.endsWith(n))
+    }
 
-        propNames.forEach(n => {
-            const desc = { ...Object.getOwnPropertyDescriptor(obj, n) }
-            delete desc.writable
-            Object.defineProperty(repl.context, n, desc)
-        })
-
-        function shouldSkipAutoCompletion(s: string) {
-            if (s.indexOf('.') < 0) {
-                return propNames.indexOf(s) < 0
+    // override completer
+    const originalCompleter = server.completer
+    server.completer = (line, callback) => {
+        originalCompleter.call(server, line, (err: any, out: [string[], string]) => {
+            if (err) {
+                return callback(err)
             }
-            return skipSuffixes.some(n => s.endsWith(n))
-        }
-
-
-        repl.completer = (line, callback) => {
-            originCompleter.call(repl, line, (err: any, out: [string[], string]) => {
-                if (err) return callback(err)
-                callback(null, [out[0].filter(i => !shouldSkipAutoCompletion(i)), out[1]])
-            })
-        }
+            callback(null, [out[0].filter(i => !shouldSkipAutoCompletion(i)), out[1]])
+        })
     }
-    process.nextTick(init)
-    repl.on('reset', init)
 
-    //override eval to support Promise    
-    const originEval = repl.eval;
-    repl.eval = (cmd, ctx, filename, callback) => {
-        async function finish(err: any, result: any) {
-            const listeners = repl.listeners('line')
-            listeners.forEach(l => repl.removeListener('line', l as any))
-            try {
-                if (err)
+    // override eval to support Promise    
+    const originalEval = server.eval;
+    server.eval = (cmd, ctx, filename, callback) => {
+        let awaitFlag = false
+        cmd = cmd.trimLeft()
+        if (cmd.startsWith('@')) {
+            cmd = cmd.substr(1).trimLeft()
+            awaitFlag = true
+        }
+        if (cmd.length === 0) {
+            return callback()
+        }
+
+        if (awaitFlag) {
+            originalEval.call(server, cmd, ctx, filename, async (err?: any, result?: any) => {
+                if (err) {
                     return callback(err)
-
-                if (result) {
-                    let resolvedResult
-                    try {
-                        resolvedResult = await result
-                    } catch (e) {
-                        return callback(e)
-                    }
-
-                    if (resolvedResult === VOID)
-                        return callback(null)
-
-                    callback(null, resolvedResult)
-                } else {
-                    if (arguments.length > 1)
-                        callback(err, result)
-                    else
-                        callback(err)
                 }
-            } finally {
-                listeners.forEach(l => repl.addListener('line', l as any))
-            }
+                const listeners = server.listeners('line')
+                server.removeAllListeners('line')
+                try {
+                    callback(undefined, await result)
+                } catch (e) {
+                    callback(e)
+                } finally {
+                    listeners.forEach(l => server.addListener('line', l as any))
+                }
+            })
+        } else {
+            originalEval.call(server, cmd, ctx, filename, callback)
         }
-        originEval.call(repl, cmd, ctx, filename, finish)
     }
-    return repl
+    return server
 }
-
-export const VOID = {}
